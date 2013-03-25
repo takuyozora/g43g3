173392507bc01d5accc96d5105cca022888910f5
Karim <ikarim@me.com>
2013-03-25 14:19:37 +0100

    MAJ package Gui et ajout de MainWindow.java

---
+++ src/gui/MainWindow.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.JFrame;
+
+// Classe de la fenetre principale 
+public class MainWindow extends JFrame {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
