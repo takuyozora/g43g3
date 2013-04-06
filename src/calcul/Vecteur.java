a81767ccba299bba867a3484428ea6b3268d5cac
Olivier Radisson <olivier.radisson@insa-lyon.fr>
2013-04-06 12:49:26 +0200

    Travail sur la classe Vecteur (oublie de ce que j'ai fait dsl)

--- src/calcul/Vecteur.java
+++ src/calcul/Vecteur.java
@@ -6,13 +6,13 @@ public class Vecteur {
 	static final int TYPE_SPHERIQUE = 11;
 	static final int TYPE_CYLINDRIQUE = 12;
 	
-	static final int OPP_PLUS = 20;
-	static final int OPP_MOINS = 21;
-	static final int OPP_PRODUIT = 22;
+	static final int OPE_PLUS = 20;
+	static final int OPE_MOINS = 21;
+	static final int OPE_PRODUIT = 22;
 	
-	public double vectx ;
-	public double vecty ;
-	public double vectz ;
+	public double x ;
+	public double y ;
+	public double z ;
 	
 	public double rayons;
 	public double thetas;
@@ -20,22 +20,25 @@ public class Vecteur {
 	
 	public double rayonr;
 	public double thetar;
-	public double vecth;
+	public double h;
 	
 	
 	public double norme ;
 	
 	public Vecteur(){
-		vectx = 0 ;
-		vecty = 0 ;
-		vectz = 0 ;
+		this.x = 0 ;
+		this.y = 0 ;
+		this.z = 0 ;
 		this.updateCylindrique(TYPE_CARTESIEN);
+		this.updateSpherique(TYPE_CARTESIEN);
 	}
 	
 	public Vecteur(double x , double y , double z){
-		vectx = x ;
-		vecty = y ;
-		vectz = z ;
+		this.x = x ;
+		this.y = y ;
+		this.z = z ;
+		this.updateCylindrique(TYPE_CARTESIEN);
+		this.updateSpherique(TYPE_CARTESIEN);
 	}
 	
 	public double norme(double x , double y , double z){
@@ -67,8 +70,30 @@ public class Vecteur {
 		}
 	}
 	
-	public Vecteur opperation(Vecteur vect1, Vecteur vect2, int oppType){
+	public static Vecteur operation(Vecteur vect1, Vecteur vect2, int opeType){
 		Vecteur result = new Vecteur();
-		
+		if (opeType == OPE_PLUS){
+			result.x = vect1.x + vect2.x;
+			result.y = vect1.y + vect2.y;
+			result.z = vect1.z + vect2.z;
+			return result;
+		}
+		else if (opeType == OPE_MOINS){
+			result.x = vect1.x - vect2.x;
+			result.y = vect1.y - vect2.y;
+			result.z = vect1.z - vect2.z;
+			return result;
+		}
+		else if (opeType == OPE_PRODUIT){
+			result.x = vect1.y * vect2.z - vect1.z * vect2.y;
+			result.y = vect1.z * vect2.x - vect1.x * vect2.z;
+			result.z = vect1.x * vect2.y - vect1.y * vect2.x;
+			return result;
+		}
+		throw new IllegalArgumentException();
+	}
+	
+	public void operation(Vecteur vect2, int opeType){
+		this = Vecteur.operation(this,vect2,opeType);
 	}
 }
