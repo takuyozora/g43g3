fb8a64e8dbfe364adce27ca57d85c73f38daea14
LEDOUX Florian <ledouxflo@gmail.com>
2013-04-08 16:02:16 +0200

    Methode impact

--- src/calcul/Espace.java
+++ src/calcul/Espace.java
@@ -2,29 +2,42 @@ package calcul;
 
 public static class Espace {
 	/*
-	 * Cette classe est là pour définire l'espace dans lequel la balle peut évoluer.
+	 * Cette classe est là pour définir l'espace dans lequel la balle peut évoluer.
 	 * Son but est de prévenir à chaque itération si la balle a touchée un des murs.
 	 */
 	
-	public static int IMPACTE_SOL = 10;
-	public static int IMPACTE_PLAFOND = 11;
-	public static int IMPACTE_NORD = 20;
-	public static int IMPACTE_SUD = 21;
-	public static int IMPACTE_EST = 22;
+	public static int IMPACT_SOL = 10;
+	public static int IMPACT_PLAFOND = 11;
+	public static int IMPACT_NORD = 20;
+	public static int IMPACT_SUD = 21;
+	public static int IMPACT_EST = 22;
 	public static int IMPACT_OUEST = 23;
+	public static int PAS_IMPACT = 30
 	
-	public static Vecteur GRAVITE = new Vecteur(0,0,9.81);
+	public static Vecteur GRAVITE = new Vecteur(0,0,-9.81);
 	
-/* KARIM : J'ai comment� pour pouvoir compiler.
+
 	
 	public static double LARGEUR = ***;
 	public static double PROFONDEUR = ***;
 	public static double HAUTEUR_HAUTE = ***;
 	public static double HAUTEUR_BASSE = ***;
 */	
-	public static int impacte(Vecteur position){
+	public static int impact(Vecteur position){
 		if(position.x < 0){
-			
+			return IMPACT_EST ;
+		}
+		
+		else if(position.y < 0){
+			return IMPACT_SUD;
+		}
+		
+		else if (position.y > PROFONDEUR){
+			return IMPACT_NORD;
+		}
+		
+		else if (position.x > LARGEUR){
+			return IMPACT_OUEST;
 		}
 	}
 
