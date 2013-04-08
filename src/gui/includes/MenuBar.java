fda57e263f9430ba9a2b51c81fbe7650570e6a79
Karim <ikarim@me.com>
2013-04-08 15:33:27 +0200

    Ajout d'icônes à la barre de menus.

--- src/gui/includes/MenuBar.java
+++ src/gui/includes/MenuBar.java
@@ -1,8 +1,14 @@
 package gui.includes;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Image;
+import java.io.IOException;
 
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -10,13 +16,25 @@ import javax.swing.JTextField;
 
 public class MenuBar extends JPanel {
 
+	String resPath = "./src/gui/res/"; // Path to resources
 	
+	/* MenuBar buttons definition w/ icon */
+	ImageIcon addIcon = new ImageIcon(resPath+"add.png");
+	JButton addButton = new JButton("Nouveau", addIcon);
+	
+	ImageIcon favsIcon = new ImageIcon(resPath+"favs.png");
+	JButton favsButton = new JButton("Favoris", favsIcon);
+
+	ImageIcon settingsIcon = new ImageIcon(resPath+"settings.png");
+	JButton settingsButton = new JButton("Paramètres", settingsIcon);
+
 	public MenuBar() {
-		this.setLayout(new FlowLayout());
-		this.add(new JButton("Nouveau"));
-		this.add(new JButton("Favoris"));
-		this.add(new JButton("Aide"));
 		
+		
+		this.setLayout(new FlowLayout());
+		this.add(addButton);
+		this.add(favsButton);
+		this.add(settingsButton);
 		this.setBackground(Color.BLACK);
 	}
 	
