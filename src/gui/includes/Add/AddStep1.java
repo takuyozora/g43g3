f58557b9a09f26d25c406682c3a0b383086b2fa4
Karim <ikarim@me.com>
2013-05-03 01:05:50 +0200

    Contenu par défaut pour l'ajout de programme à l'étape 1.

--- src/gui/includes/Add/AddStep1.java
+++ src/gui/includes/Add/AddStep1.java
@@ -1,7 +1,14 @@
 package gui.includes.Add;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 public class AddStep1 extends JPanel {
-
+	
+	public static AddStep1 panel = new AddStep1();
+	
+	public AddStep1() {
+		this.add(new JLabel("Add Step 1", 10));
+		
+	}
 }
