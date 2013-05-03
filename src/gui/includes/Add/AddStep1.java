394429e95df21653e43eb0a740a0256af79b3455
Karim <ikarim@me.com>
2013-05-03 23:02:27 +0200

    Modifications diverses.

--- src/gui/includes/Add/AddStep1.java
+++ src/gui/includes/Add/AddStep1.java
@@ -1,39 +1,49 @@
 package gui.includes.Add;
 
 import gui.MainWindow;
-import gui.includes.MenuBar;
 import gui.includes.Terrain.Terrain;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-
-import com.jogamp.opengl.util.FPSAnimator;
+import javax.swing.JTextField;
 
 public class AddStep1 extends JPanel {
 	
 	public static AddStep1 panel = new AddStep1();
-	private JButton voirterrain = new JButton("Voir terrain");
-	private JPanel terrainContainer = new JPanel();
+	
+	private JLabel title = new JLabel("Step 1", 10);
+	private JPanel formContainer = new JPanel();
+	private JLabel progNameDescription = new JLabel("Nom du programme : ", 10);
+	private JTextField progName = new JTextField(1);
+	
+	private JButton next = new JButton("Suivant");
+	
 	
 	public AddStep1() {
-		this.add(new JLabel("Add Step 1", 10));
-		this.add(voirterrain);
+		this.setLayout(new BorderLayout());
+		this.add(title, BorderLayout.NORTH);
+		this.add(formContainer, BorderLayout.CENTER);
+		formContainer.setLayout(new BorderLayout());
+		formContainer.add(progNameDescription, BorderLayout.NORTH);
+		formContainer.add(progName, BorderLayout.NORTH);
+		formContainer.add(next, BorderLayout.CENTER);
+
+	//	this.add(voirterrain); Bouton pour voir le terrain - test uniquement
 		
-		voirterrain.addActionListener(new ActionListener() { 
+		next.addActionListener(new ActionListener() { 
 			public void actionPerformed(ActionEvent e) { 
-				voirterrainPressed();
+				nextPressed();
 			} 
 		});
 		
 	}
 	
-	public void voirterrainPressed() {
+	public void nextPressed() {
 		MainWindow.fenetre.buildWindow(Terrain.terrain);
 		Terrain.terrain.requestFocusInWindow();
 	}
